be1e568c83ca54afcb4c50ce1af231c318d87844
Gattai <108435021+BrunoGattai@users.noreply.github.com>
2023-04-19 16:15:52 -0300

    Update Currency and TradeSystem classes

--- Scripts/Currency.cs
+++ Scripts/Currency.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 namespace GattaiEconomySystem
 {
     /// <summary>
-    /// A class that represents a currency which balance can be added and spent.
+    /// Represents a currency that has a balance, symbol, and icon
     /// </summary>
     [Serializable]
     public class Currency
     {
-        /// <summary>
-        /// The current balance of the currency.
-        /// </summary>
-        [SerializeField] private int balance;
+        [SerializeField, Tooltip("The initial balance of the currency")] 
+        private int initialBalance;
+        
+        [SerializeField, Tooltip("The current balance of the currency")] 
+        private int balance;
+        
+        [SerializeField, Tooltip("The symbol of the currency")] 
+        private char symbol;
+        
+        [SerializeField, Tooltip("The icon of the currency")] 
+        private Sprite icon;
+
+        [SerializeField, Tooltip("Whether this is a soft currency, usually soft currencies can be acquired playing " +
+                                 "the game without expending real money for it.")]
+        private bool isSoftCurrency = true;
         
         /// <summary>
         /// Gets the current balance of the currency.
@@ -20,21 +31,46 @@ namespace GattaiEconomySystem
         public int Balance => balance;
 
         /// <summary>
+        /// Gets the symbol of the Currency.
+        /// </summary>
+        public char Symbol => symbol;
+
+        /// <summary>
+        /// Gets the icon of the Currency.
+        /// </summary>
+        public Sprite Icon => icon;
+
+        /// <summary>
+        /// Gets the boolean field that represents whether this is a soft currency.
+        /// Usually soft currencies can be acquired playing the game without expending real money for it.
+        /// </summary>
+        public bool IsSoftCurrency => isSoftCurrency;
+
+        /// <summary>
         /// The minimum balance value allowed for the currency.
         /// </summary>
-        public const int MINIMUM_BALANCE = 0;
+        public virtual int MinimumBalance { get; set; }
         
         /// <summary>
         /// The maximum balance value allowed for the currency.
         /// </summary>
-        public const int MAXIMUM_BALANCE = 9999999;
+        public virtual int MaximumBalance { get; set; } = 9999999;
 
+        /// <summary>
+        /// Creates a new instance of the Currency class.
+        /// </summary>
+        public Currency()
+        {
+            balance = initialBalance;
+        }
+        
         /// <summary>
         /// Creates a new instance of the Currency class with the specified initial balance.
         /// </summary>
         /// <param name="initialBalance">The initial balance for the currency.</param>
         public Currency(int initialBalance)
         {
+            this.initialBalance = initialBalance;
             balance = initialBalance;
         }
 
@@ -43,7 +79,7 @@ namespace GattaiEconomySystem
         /// maximum or go below the minimum value.
         /// </summary>
         /// <param name="value">The value to add to the balance.</param>
-        public void AddToBalance(int value)
+        public virtual void AddToBalance(int value)
         {
             balance += value;
             ClampBalance();
@@ -55,7 +91,7 @@ namespace GattaiEconomySystem
         /// </summary>
         /// <param name="value">The value to check and subtract from the balance.</param>
         /// <returns>Whether can afford for the value or not.</returns>
-        public void SubtractFromBalance(int value)
+        public virtual void SubtractFromBalance(int value)
         {
             balance -= value;
             ClampBalance();
@@ -67,7 +103,7 @@ namespace GattaiEconomySystem
         /// </summary>
         /// <param name="value">The value to check and subtract from the balance.</param>
         /// <returns>Whether can afford for the value or not.</returns>
-        public bool CanAfford(int value)
+        public virtual bool CanAfford(int value)
         {
             return value <= balance;
         }
@@ -75,9 +111,13 @@ namespace GattaiEconomySystem
         /// <summary>
         /// Clamps the currency balance to the minimum and maximum values allowed.
         /// </summary>
-        private void ClampBalance()
-        {
-            balance = Mathf.RoundToInt(Mathf.Clamp(balance, MINIMUM_BALANCE, MAXIMUM_BALANCE));
-        }
+        private void ClampBalance() => balance = Mathf.RoundToInt(Mathf.Clamp(balance, MinimumBalance, MaximumBalance));
+
+        /// <summary>
+        /// Gets the balance concatenated with the symbol.
+        /// </summary>
+        /// <param name="symbolAtFront">False by default. If true it makes the symbol be concatenated before the balance.</param>
+        /// <returns>The concatenated balance with the symbol.</returns>
+        public string GetFormattedBalance(bool symbolAtFront = false) => symbolAtFront ? $"{symbol} {balance}" : $"{balance} {symbol}";
     }
 }
--- Scripts/TradeSystem.cs
+++ Scripts/TradeSystem.cs
@@ -1,22 +1,44 @@
+using UnityEngine;
+
 namespace GattaiEconomySystem
 {
+    /// <summary>
+    /// A static class that handles trade operations, specifically buying and selling things using currencies.
+    /// </summary>
     public static class TradeSystem
     {
-        public static bool AttemptToBuy(EconomyEntity buyer, EconomyEntity seller, int price)
+        private static bool IsValidOperation(Currency buyerCurrency, Currency sellerCurrency) => 
+            buyerCurrency.GetType() != sellerCurrency.GetType() || buyerCurrency.IsSoftCurrency != sellerCurrency.IsSoftCurrency;
+
+        /// <summary>
+        /// Attempts to buy a thing from the seller using the currency of the buyer for the specified price.
+        /// </summary>
+        /// <param name="buyerCurrency">The currency of the buyer.</param>
+        /// <param name="sellerCurrency">The currency of the seller.</param>
+        /// <param name="price">The price of the thing.</param>
+        /// <returns>Returns `true` if the purchase was successful, and `false` if the buyer does not have enough funds to buy the thing.</returns>
+        public static bool AttemptToBuy(Currency buyerCurrency, Currency sellerCurrency, int price)
         {
-            if (buyer.Currency.CanAfford(price))
+            if (IsValidOperation(buyerCurrency, sellerCurrency))
             {
-                buyer.Currency.SubtractFromBalance(price);
-                seller.Currency.AddToBalance(price);
-                return true;
+                Debug.LogError("Attempt to make a operation using different currencies or soft currency settings is not allowed!");
+                return false;
             }
-
-            return false;
+            
+            if (!buyerCurrency.CanAfford(price)) return false;
+            buyerCurrency.SubtractFromBalance(price);
+            sellerCurrency.AddToBalance(price);
+            return true;
         }
 
-        public static bool AttemptToSell(EconomyEntity seller, EconomyEntity buyer, int price)
-        {
-            return AttemptToBuy(buyer, seller, price);
-        }
+        /// <summary>
+        /// Attempts to sell a thing to a buyer using the currency of the seller for the specified price.
+        /// </summary>
+        /// <param name="sellerCurrency">The currency of the seller.</param>
+        /// <param name="buyerCurrency">The currency of the buyer.</param>
+        /// <param name="price">The price of the thing.</param>
+        /// <returns>Returns `true` if the sale was successful, and `false` if the buyer does not have enough funds to buy the thing.</returns>
+        public static bool AttemptToSell(Currency sellerCurrency, Currency buyerCurrency, int price) => 
+            AttemptToBuy(buyerCurrency, sellerCurrency, price);
     }
 }
